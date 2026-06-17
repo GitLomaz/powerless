e064bf9941cb03b0744db296128ac704cf0b51cf
Ian Lomas <you@example.com>
2026-06-17 12:12:20 -0400

    minigun

---
+++ js/classes/minibullet.js
@@ -0,0 +1,19 @@
+class MiniBullet extends Phaser.GameObjects.Container {
+  constructor(target) {
+    super(scene, scene.player.x, scene.player.y);
+    this.circle = scene.add.circle(0, 0, 2, 0x000000);
+    this.add(this.circle);
+    scene.add.existing(this);
+    this.speed = gameState.upgrades.minigun.fireRate;
+    this.damage = gameState.upgrades.minigun.damage;
+    this.range = gameState.upgrades.minigun.range;
+    scene.bulletGroup.add(this);
+    this.body.setCircle(2, -2, -2);
+    this.velocity = new Phaser.Math.Vector2(target.x - this.x, target.y - this.y).normalize().scale(this.speed);
+    this.body.setVelocity(this.velocity.x, this.velocity.y);
+
+    scene.time.delayedCall(this.range, () => {
+      this.destroy();
+    });
+  }
+}
